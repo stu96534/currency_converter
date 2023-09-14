9059297c371e9f93ac0b717f5fb1615eec5054c2
stu96534 <stu96534@gmail.com>
2023-09-14 20:29:05 +0800

    feat: create helpers file & add currency_helpers.js

---
+++ helpers/currency_helpers.js
@@ -0,0 +1,24 @@
+
+const currency = (target, arr) => {
+  for (let i = 0; i < target.length; i++) {
+    arr.push(target[i].children[0].data.trim())
+  }
+}
+
+const rateOfExchange = (target, arrBuy, arrSell) => {
+  for (let i = 0; i < target.length; i++) {
+    if (i % 2 === 0) {
+      arrBuy.push(target[i].children[0].data.trim())
+    } else {
+      arrSell.push(target[i].children[0].data.trim())
+    }
+  }
+}
+
+
+
+
+module.exports = {
+  currency,
+  rateOfExchange
+}
