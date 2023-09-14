a66a62872e17157f95c061fd8ffeb58848c3ae3a
stu96534 <stu96534@gmail.com>
2023-09-14 20:37:44 +0800

    feat: add apis route to use

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 
+const apis = require('./routes/apis')
+
 const app = express()
 
 const port = process.env.PORT || 3000
+
+app.use('/api', apis)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
