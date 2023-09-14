141f4407ec5f2890e37ecd001b8386b8413516d9
stu96534 <stu96534@gmail.com>
2023-09-14 20:41:36 +0800

    feat: add currencyController to routes/modules/taiwanBank.js

--- routes/apis/modules/taiwanBank.js
+++ routes/apis/modules/taiwanBank.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const currencyController = require('../../controllers/currency-controller')
 
-
+router.get('/currency', currencyController.getTaiwanBankRate)
 
 module.exports = router
