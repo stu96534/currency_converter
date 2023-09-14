4a6985ccb819168a41b0db3df47c25e3143b153c
stu96534 <stu96534@gmail.com>
2023-09-14 21:32:38 +0800

    feat: add getTaiwanBankRate function to currencyController

--- controllers/currency-controller.js
+++ controllers/currency-controller.js
@@ -1,9 +1,28 @@
-
+const { TaiwanBank } = require('../models')
 const { taiwanBank } = require('../lib/taiwanBankExchangeRate')
 
 const currencyController = {
-  getTaiwanBankRate: (req, res, next) => {
-    taiwanBank()
+  getTaiwanBankRate: async (req, res, next) => {
+    try {
+
+      const currencies = await TaiwanBank.findAndCountAll({})
+
+      const data = currencies.rows.map(currency => ({
+        name: currency.name,
+        cashBuy: currency.cashBuy,
+        cashSell: currency.cashSell,
+        sightBuy: currency.sightBuy,
+        sightSell: currency.sightSell
+      }))
+
+      return res.status(200).json({
+        currencies: data
+      })
+
+    } catch (err) {
+      next(err)
+    }
+
   }
 }
 
