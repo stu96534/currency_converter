cb4ffb4c1790b4c9511a8d6e645be87d2bbea0ed
stu96534 <stu96534@gmail.com>
2023-11-27 16:06:19 +0800

    feat: setting production mode database

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    username: "root",
+    password: "password",
+    database: "currency_converter",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "currency_converter_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    username: process.env.PROD_USERNAME,
+    password: process.env.PROD_PASSWORD,
+    database: process.env.PROD_DATABASE,
+    host: process.env.PROD_HOST,
+    port: process.env.PROD_PORT,
+    dialect: "mysql"
+  }
+};
